2711ce189c68b209e8efd9fb32eea8efb02558d5
sondos-ahmed <sondos.betamer@gmail.com>
2022-12-06 14:57:02 +0000

    begin with article page

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,16 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getArticleById } from "../api.js";
 import { useParams } from "react-router-dom";
 
 function Article() {
+  const [article, setArticle] = useState();
   const { article_id } = useParams();
 
   useEffect(() => {
-    getArticleById(article_id).then(({ data: { article } }) => {});
+    getArticleById(article_id).then(({ data: { article } }) => {
+      console.log(article);
+      setArticle(article);
+    });
   }, [article_id]);
   return (
     <section>
-      <h3>Under development </h3>
+      <h3>{article?.title}</h3>
     </section>
   );
 }
